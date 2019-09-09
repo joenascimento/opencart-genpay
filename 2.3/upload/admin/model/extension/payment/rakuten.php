9269ac67f7d753871e228ed87cd80d8cbbed697b
Joe Pereira Nascimento <joenascimento2000@gmail.com>
2019-09-08 21:53:39 -0300

    removendo métodos que não serão necessários e alterando o nome da tabela sem o prefixo

--- 2.3/upload/admin/model/extension/payment/rakuten.php
+++ 2.3/upload/admin/model/extension/payment/rakuten.php
@@ -1,4 +1,5 @@
 <?php
+
 class ModelExtensionPaymentRakuten extends Model {
 
     /**
@@ -10,28 +11,6 @@ class ModelExtensionPaymentRakuten extends Model {
      * SANDBOX API URL.
      */
     const SANDBOX_API_URL = 'https://oneapi-sandbox.rakutenpay.com.br/rpay/v1/';
-
-    /**
-     * PRODUCTION_JS_URL
-     */
-    const PRODUCTION_JS_URL = 'https://static.rakutenpay.com.br/rpayjs/rpay-latest.min.js';
-
-    /**
-     * SANDBOX_JS_URL
-     */
-    const SANDBOX_JS_URL = 'https://static.rakutenpay.com.br/rpayjs/rpay-latest.dev.min.js';
-
-    /**
-     * Get Configuration Document
-     *
-     * @return string  CPF/CNPJ
-     */
-    public function getConfDocument()
-    {
-        $document = $this->config->get('payment_rakuten_document');
-
-        return $document;
-    }
 
     /**
      * Check if Rakuten Pay response is valid.
@@ -75,39 +54,6 @@ class ModelExtensionPaymentRakuten extends Model {
     }
 
     /**
-     * Get environment
-     *
-     * @return array   sandbox/production API/JS
-     */
-    public function getEnvironment($environment)
-    {
-
-        $api = $this->getApiUrl($environment);
-        $js = $this->getJsUrl();
-
-        $this->environment = $environment;
-        $this->api = $api;
-        $this->rpay_js = $js;
-
-        if ( 'production' === $this->environment ) {
-
-            return [
-                'place' => $this->environment,
-                'api' => $this->api,
-                'rpay_js' => $this->rpay_js,
-            ];
-
-        }
-
-        return [
-            'place' => $this->environment,
-            'api' => $this->api,
-            'rpay_js' => $this->rpay_js,
-        ];
-
-    }
-
-    /**
      * Get API URL.
      *
      * @return string
@@ -127,31 +73,11 @@ class ModelExtensionPaymentRakuten extends Model {
         }
     }
 
-    /**
-     * Get JS Library URL.
-     *
-     * @return string
-     */
-    private function getJsUrl() {
-
-        $this->environment = $this->config->get('payment_rakuten_environment');
-
-        if ( 'production' === $this->environment ) {
-
-            return self::PRODUCTION_JS_URL;
-
-        } else {
-
-            return self::SANDBOX_JS_URL;
-
-        }
-    }
-
     public function install() {
         $this->db->query("INSERT INTO `" . DB_PREFIX . "extension` (`type`, `code`) VALUES ('payment', 'rakuten_boleto') ");
         $this->db->query("INSERT INTO `" . DB_PREFIX . "extension` (`type`, `code`) VALUES ('payment', 'rakuten_cartao') ");
         $this->db->query("
-          CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "rakutenpay_orders` ( 
+          CREATE TABLE IF NOT EXISTS `rakutenpay_orders` ( 
               `id` INT(20) NOT NULL AUTO_INCREMENT , 
               `order_id` INT(20) NOT NULL , 
               `charge_uuid` VARCHAR(80) NOT NULL , 
