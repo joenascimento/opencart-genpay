ad98c6a807bf109037853e377bb97925d2db8df2
Joe Nascimento <joe.nascimento@rakuten.com.br>
2019-09-18 16:29:26 -0300

    .

--- 2.3/upload/catalog/language/en-gb/extension/payment/rakuten.php
+++ 2.3/upload/catalog/language/en-gb/extension/payment/rakuten.php
@@ -3,8 +3,8 @@
 $_['text_boleto']    = 'Rakuten Boleto';
 $_['text_cartao']    = 'Rakuten Cartão de Crédito';
 
-$_['rakuten_success'] = '<p>O <strong>Pedido %s</strong> foi processado com sucesso e o status do pedido é %s. <br>Você pode visualizar o histórico de pedidos acessando a página <a href="%s">minha conta</a>.</p>';
-$_['rakuten_failure'] = '<p>Infelizmente seu pedido foi cancelado, favor ligar para sua operadora e verificar o problema</p>';
+$_['rakuten_success'] = '<p>O <strong>Pedido %s</strong> foi recebido e o status do pedido é %s. <br>Você pode visualizar o histórico de pedidos acessando a página <a href="%s">minha conta</a>.</p>';
+$_['rakuten_failure'] = '<p>O pedido %s ficou com o status %s, favor ligar para sua operadora e verificar o problema</p>';
 
 $_['rakuten_title_success'] = 'Seu pedido foi realizado';
-$_['rakuten_title_failure'] = 'Ocorreu um erro no pedido';
+$_['rakuten_title_failure'] = 'Infelizmente seu pedido foi cancelado';
