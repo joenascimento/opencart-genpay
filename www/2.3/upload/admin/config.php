4c9f06154c6d33df0153cd77f5b759f65032e935
Joe Pereira Nascimento <joenascimento2000@gmail.com>
2019-10-09 23:03:35 -0300

    .

---
+++ www/2.3/upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/2.3/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/2.3/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/2.3/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/2.3/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/2.3/upload/admin/');
+define('DIR_SYSTEM', '/var/www/html/2.3/upload/system/');
+define('DIR_IMAGE', '/var/www/html/2.3/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/2.3/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/2.3/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/2.3/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/2.3/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/2.3/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/2.3/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/2.3/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/2.3/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/2.3/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db-opencart');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'linux');
+define('DB_DATABASE', 'dev');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc23_');
---
+++ www/2.3/upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/2.3/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/2.3/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/2.3/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/html/2.3/upload/system/');
+define('DIR_IMAGE', '/var/www/html/2.3/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/2.3/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/2.3/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/2.3/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/2.3/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/2.3/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/2.3/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/2.3/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/2.3/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db-opencart');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'linux');
+define('DB_DATABASE', 'dev');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc23_');
